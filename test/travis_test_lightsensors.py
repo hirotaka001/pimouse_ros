e1fd56e44880d482a49dd7b7e74abf5c58de64cf
Hiroaki Takahashi <hrktkhsh63784@gmail.com>
2019-03-12 14:18:48 +0000

    Modify travis_test_lightsensors.py and travis_test_motors1.py

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#encoding utf8
+#encoding: utf8
 import unittest, rostest
 import rosnode, rospy
 import time
--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#encoding utf8
+#encoding: utf8
 import unittest, rostest
 import rosnode, rospy
 import time
@@ -8,7 +8,7 @@ from geometry_msgs.msg import Twist
 
 class MotorTest(unittest.TestCase):
     def file_check(self, dev, value, message):
-        with open("/dev" + dev, "r") as f:
+        with open("/dev/" + dev, "r") as f:
             self.assertEqual(f.readline(), str(value)+"\n", message)
 
     def test_node_exist(self):
