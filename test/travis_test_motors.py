74f0f862c4e7a983c03899adea92c845714f7b80
Hiroaki Takahashi <hrktkhsh63784@gmail.com>
2019-03-17 03:47:00 +0000

    Modify travis_test_motors.py

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -12,7 +12,7 @@ class MotorTest(unittest.TestCase):
     def setUp(self):
         rospy.wait_for_service('/motor_on')
         rospy.wait_for_service('/motor_off')
-        rospy.wait_for_service('/timed_motion")
+        rospy.wait_for_service('/timed_motion')
         on = rospy.ServiceProxy('/motor_on', Trigger)
         ret = on()
 
